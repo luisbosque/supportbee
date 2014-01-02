e565b1a325d4ff1a4bb5101132fc21976139e9ca
Luis Bosque <luisico@gmail.com>
2014-01-02 13:34:43 +0100

    Double check in order to return some wrong tickets

--- lib/supportbee/client/tickets.rb
+++ lib/supportbee/client/tickets.rb
@@ -3,7 +3,27 @@ module Supportbee
     module Tickets
       
       def tickets(extra_parameters = {})
-        index('tickets', extra_parameters) 
+        # Default parameters according to 
+        # https://developers.supportbee.com/api
+        if extra_parameters['archived'].nil?
+          extra_parameters['archived'] = false
+        end
+        if extra_parameters['spam'].nil?
+          extra_parameters['spam'] = false
+        end
+        if extra_parameters['trash'].nil?
+          extra_parameters['trash'] = false
+        end
+        tickets = index('tickets', extra_parameters) 
+        # Dirty trick to avoid getting wrong tickets in case the API doesn't
+        # honor some parameters
+        known_filters = ['spam', 'trash', 'archived']
+        filters = extra_parameters.select do |p| 
+          known_filters.include?(p)
+        end
+        tickets.select do |t|
+          !filters.map {|k,v| t[k].to_s == v.to_s}.include?(false)
+        end
       end
 
       def ticket(id)
